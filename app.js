e12f2e35f72468598bc966d19546cb155d99b490
Kaustubh Manglurkar <kaustubh1517@gmail.com>
2021-08-08 15:38:44 +0530

    exercise 1 done

---
+++ app.js
@@ -0,0 +1,33 @@
+const inputs = document.querySelectorAll(".input");
+const outputDiv = document.querySelector("#output");
+
+const btn = document.querySelector("#check-btn");
+
+
+
+function sumOfAngles() {
+
+    const sum = Number(inputs[0].value) + Number(inputs[1].value) + Number(inputs[2].value);
+
+    return sum;
+
+
+}
+
+function checkTriangle() {
+
+    const storeSum = sumOfAngles()
+
+    if(storeSum === 180){
+
+        outputDiv.innerText = "Yay, the angles forms a Triangle!";
+    }
+
+    else {
+
+        outputDiv.innerText = "Oops! It does not form a triangle";
+    }
+  
+}
+
+btn.addEventListener("click", checkTriangle);
