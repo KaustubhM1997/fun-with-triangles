c1a0cc2a67bbe9db3f1bb0f88d278ab24524386e
Kaustubh Manglurkar <kaustubh1517@gmail.com>
2021-08-10 08:08:11 +0530

    html and logic done

---
+++ area.js
@@ -0,0 +1,25 @@
+const areaInputs = document.querySelectorAll(".input");
+
+const areaBtn = document.querySelector("#calculate-area");
+
+const outputDiv = document.querySelector("#output");
+
+function findArea() {
+
+    const area = calculateArea(Number(areaInputs[0].value), Number(areaInputs[1].value));
+
+    outputDiv.innerText = "The area of the triangle is " + area;
+    
+}
+
+function calculateArea(a, b) {
+
+    const area = 0.5 * a * b;
+
+   return area;
+}
+
+
+
+
+areaBtn.addEventListener("click", findArea);
---
+++ hypotenuse.js
@@ -0,0 +1,33 @@
+const inputs = document.querySelectorAll(".input")
+const calculateBtn  = document.querySelector("#calculate");
+const outputDiv = document.querySelector("#output");
+
+
+function calculateHypotenuse() {
+
+    const storeSum = calculateSum(Number(inputs[0]. value), Number(inputs[1].value));
+
+    const lengthOfHypotenuse = Math.sqrt(storeSum);
+
+    outputDiv.innerText = "The length of the hypotenuse is " + lengthOfHypotenuse;
+
+
+    
+
+ 
+
+
+}
+
+function calculateSum(a,b) {
+
+    const sumOfSquares = a*a + b*b;
+
+    return sumOfSquares;
+
+}
+
+
+
+
+calculateBtn.addEventListener("click", calculateHypotenuse);
